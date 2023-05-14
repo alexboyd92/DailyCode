f3afaec052cfd266e9410c60a2d92c6ce638bacd
alexb <alexanderboyd92@gmail.com>
2023-05-13 22:22:22 -0400

    Started AddTwoNumbers. Moved menu into its own function for testing

--- src/Driver.java
+++ src/Driver.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) {
+        AddTwoNumbers.getInput();
+        //menu();
+
+
+    }
+    public static void menu(){
         Scanner userInput= new Scanner(System.in);
         System.out.println("Please choose which to run\n" +
                 "1) Two sum problem \n" +
-                "2) Parenthesis generator" );
+                "2) Parenthesis generator\n" +
+                "3) Add two Numbers" );
         int selection = userInput.nextInt();
         switch(selection){
             case 1:
@@ -15,13 +22,13 @@ public class Driver {
             case 2:
                 ParenthesesGenerator.getResult();
                 break;
+            case 3:
+                AddTwoNumbers.getInput();
+                break;
             default:
                 System.out.println("No choice matches selection");
         }
         userInput.close();
-
-
-
 
     }
 }
