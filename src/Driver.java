3997325854aa70c6baf0034b3fde6735a267306c
alexb <alexanderboyd92@gmail.com>
2023-05-15 20:37:06 -0400

    Added basics of remove duplicate integer from array

--- src/Driver.java
+++ src/Driver.java
@@ -14,13 +14,15 @@ public class Driver {
                 1) Two sum problem\s
                 2) Parenthesis generator
                 3) Add two Numbers
-                4) Palindrome number checking""");
+                4) Palindrome number checking
+                5) Remove duplicate integeres from array""");
         int selection = userInput.nextInt();
         switch (selection) {
             case 1 -> twoSum.twoSum();
             case 2 -> ParenthesesGenerator.getResult();
             case 3 -> AddTwoNumbers.getInput();
             case 4 -> PalindromeNumber.getinput();
+            case 5-> RemoveDup.getArray();
             default -> System.out.println("No choice matches selection");
         }
         userInput.close();
---
+++ src/RemoveDup.java
@@ -0,0 +1,48 @@
+package src;
+
+
+import java.util.Scanner;
+
+public class RemoveDup {
+    static int removeDuplicates(int[] toClean,int size)
+    {
+        int index = 1;
+        // If array is empty or contains only one value is no duplicates exist
+
+        if (size == 0 || size == 1)
+            return size;
+        for(int i = 0; i < size - 1; i++) {
+
+            if(toClean[i] != toClean[i + 1]){
+                toClean[index] = toClean[i + 1];
+                index++;
+            }
+        }
+        return index;
+
+
+    }
+
+    // Driver code
+    public static void getArray()
+    {
+        Scanner userInput= new Scanner(System.in);
+
+        System.out.println("Please enter array size");
+        int size = userInput.nextInt();
+        int[] toClean= new int[size];
+        System.out.println("Please enter array in ascending order");
+        for (int i=0;i<size;i++){
+            toClean[i]=userInput.nextInt();
+        }
+
+        // removeDuplicates() returns new size of array
+        int arraySize = removeDuplicates(toClean,size);
+
+        // Print updated array
+        for (int i = 0; i < arraySize; i++)
+            System.out.print(toClean[i] + " ");
+    }
+}
+
+
