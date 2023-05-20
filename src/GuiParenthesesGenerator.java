c59f4af56ff2c1054a3a185cfdd591113d64a011
alexb <alexanderboyd92@gmail.com>
2023-05-20 17:15:03 -0400

    Updated gui using intelij's form

--- src/GuiParenthesesGenerator.java
+++ src/GuiParenthesesGenerator.java
@@ -8,12 +8,18 @@ import java.awt.event.ActionListener;
 public class GuiParenthesesGenerator extends JFrame implements ActionListener {
     private JButton run;
     private JLabel instructions;
-    private JLabel inputPanel;
     private JLabel result;
     private JTextField input;
+    private JPanel inputPanel;
     private JButton close;
+    private JPanel buttonPanel;
+    private JPanel resultPanel;
+    private JPanel panel;
+    private JLabel label2;
 
     public GuiParenthesesGenerator() {
+        this.setTitle("Parenthesis Generator");
+
         //Create a jframe, so we have some place to put things
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,46 +27,16 @@ public class GuiParenthesesGenerator extends JFrame implements ActionListener {
         this.setSize(600, 250);
         this.setLocationRelativeTo(null);
 
-        //Create a panel for the input text field and instructions
-
-        inputPanel = new JLabel();
-        inputPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
-
-        // Create a jlable for instructions
-        instructions = new JLabel();
-        instructions.setText("Enter Number of pairs of parentheses");
-
-        // Create a text field, so we can get input from user
-        input = new JTextField();
-        input.setPreferredSize(new Dimension(250, 20));
 
 
-        //Create a text box, so we can show the output/ warnings
-        result = new JLabel();
-        result.setVisible(false);
-        //Create a run button
-        run = new JButton("Submit");
-        run.addActionListener(this);
+       this.add(panel);
 
-        // create a button to replace run to close the program
-        close = new JButton("Close");
+
+
         close.addActionListener(this);
-        close.setVisible(false);
-
-
-        // add things to the input panel
-        inputPanel.add(instructions);
-        inputPanel.add(input);
-        inputPanel.add(result);
-        inputPanel.add(close);
-        run.setVisible(true);
-        // add panels to the frame
-        this.add(inputPanel, BorderLayout.CENTER);
-        this.add(run, BorderLayout.SOUTH);
-        // this.add(close,BorderLayout.SOUTH);
+        run.addActionListener(this);
         this.setVisible(true);
-
-
+//
     }
 
     @Override
@@ -68,10 +44,13 @@ public class GuiParenthesesGenerator extends JFrame implements ActionListener {
         if (run == e.getSource()) {
             try {
                 int n = Integer.parseInt(input.getText());
-                result.setText(ParenthesesGenerator.getResult(n));
+                label2.setText("Here are all the well-formed options for "+ n +
+                        " pairs of parentheses\n");
+                result.setText("<HTML>"
+                        +ParenthesesGenerator.getResult(n));
                 result.setForeground(Color.BLACK);
                 result.setVisible(true);
-                run.setVisible(false);
+                run.setEnabled(false);
                 close.setVisible(true);
                 input.setVisible(false);
                 instructions.setVisible(false);
